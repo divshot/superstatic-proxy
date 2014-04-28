3cc46ea91086a54f918cb68ed5b63d3d4e9a1a4c
Scott Corgan <scottcorgan@gmail.com>
2014-04-28 16:03:28 -0700

    Use node-http-proxy for all requests

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ var proxy = httpProxy.createProxyServer({});
 
 var DEFAULT_TIMEOUT = 3000;
 
-
 module.exports = function (req, res, next) {
-  req.url = req.service.path;
+  if (!req.service || !req.service.config) return next();
   
+  var config = req.service.config;
+  
+  // Set relative path
+  req.url = req.service.path || req.url; // TODO: test this "OR"
+  
+  // Set headers
+  Object.keys(config.headers).forEach(function (key) {
+    req.headers[key.toLowerCase()] = req.headers[key.toLowerCase()] || req.service.config.headers[key];
+  });
+  
+  // Set or unset cookies
+  if (config.cookies === false) delete req.headers.cookie;
+  
+  // Send proxy
   proxy.web(req, res, {
-    target: req.service.config.origin
+    target: config.origin,
+    timeout: config.timeout || DEFAULT_TIMEOUT
   });
 };
